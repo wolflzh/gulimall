613e59843fd4673f05ae3df974c613addb6821d1
Liu <304911790@qq.com>
2022-10-17 15:00:05 +0800

    添加项目模块

---
+++ gulimall-coupon/src/main/java/com/lzh/gulimall/coupon/GulimallCouponApplication.java
@@ -0,0 +1,11 @@
+package com.lzh.gulimall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+}
---
+++ gulimall-coupon/src/main/java/com/lzh/gulimall/coupon/test.java
@@ -0,0 +1,4 @@
+package com.lzh.gulimall.coupon;
+
+public class test {
+}
